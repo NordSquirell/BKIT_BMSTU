da9378bccdb8a27c8eaa42d118cbbd2304ad0283
NordSquirell <76106801+NordSquirell@users.noreply.github.com>
2021-12-23 12:21:48 +0300

    add lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -44,13 +44,19 @@ def get_roots(a, b, c):
     D = b * b - 4 * a * c
     if D == 0.0:
         root = -b / (2.0 * a)
-        result.append(root)
+        result.append(math.sqrt(root))
+        if root != 0.0:
+            result.append(-math.sqrt(root))
     elif D > 0.0:
         sqD = math.sqrt(D)
-        root1 = (-b + sqD) / (2.0 * a)
-        root2 = (-b - sqD) / (2.0 * a)
+        root1 = math.sqrt((-b + sqD) / (2.0 * a))
+        root2 = math.sqrt((-b - sqD) / (2.0 * a))
         result.append(root1)
         result.append(root2)
+        if root1 != 0.0:
+            result.append(-root1)
+        if root2 != 0.0:
+            result.append(-root2)
     return result
 
 
@@ -76,6 +82,3 @@ def main():
 # Если сценарий запущен из командной строки
 if __name__ == "__main__":
     main()
-
-# Пример запуска
-# qr.py 1 0 -4
